50ff36e038a922095dcdec43bc2c2fe424786276
Tomek <tdyda@doublecodestudio.com>
2026-01-16 12:57:37 +0100

    feat(worker): dodaj konsumenta kolejki i wyjątek permanentny
    Refs: #10

---
+++ Pixario.Ingest.Worker/Exceptions/PermanentProcessingException.cs
@@ -0,0 +1,7 @@
+namespace Pixario.Ingest.Worker.Exceptions;
+
+public sealed class PermanentProcessingException : Exception
+{
+    public PermanentProcessingException(string message) : base(message) { }
+    public PermanentProcessingException(string message, Exception inner) : base(message, inner) { }
+}
